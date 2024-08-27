c4abac9bf29f1a528702b9e764471eb916d587d0
uhammadziyo <DilafruzIminova71@gmail.com>
2024-08-27 10:06:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let color = prompt(`1-7gacha son kiriting`)
+switch (color) {
+    case 1:
+        alert(`dushanba`)
+        break;
+    case 2:
+        alert(`seshanba `)
+        break;
+    case 3:
+        alert('chorshanba')
+        break;
+    case 4:
+        alert(`payshanba`)
+        break;
+    case 5:
+            alert(`juma`)
+            break;
+    case 6:
+        alert(`shanba`)
+        break;
+        case 7:
+            alert(`yakshanba`)
+            break;
+    default:
+        alert(`bir haftada 7 kun bor`)
+}
